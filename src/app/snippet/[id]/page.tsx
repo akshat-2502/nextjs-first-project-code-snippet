3c132049ee5701d5e6f1cefaa3958ec74c6164dc
akshat-2502 <khulbeakshat@gmail.com>
2025-05-19 20:59:50 +0530

    delete function completed

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { prisma } from "@/lib/prisma";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import * as actions from "@/actions/saveRequest";
 
 type PropDetails = {
   params: { id: string };
@@ -18,6 +19,8 @@ const SnippetDetailPage = async ({ params }: PropDetails) => {
       id,
     },
   });
+
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
   return (
     <div className="flex flex-col gap-5">
       <div className="flex items-center justify-between">
@@ -26,7 +29,11 @@ const SnippetDetailPage = async ({ params }: PropDetails) => {
           <Link href={`/snippet/${snippet.id}/edit`}>
             <Button>Edit</Button>
           </Link>
-          <Button variant={"destructive"}>Delete</Button>
+          <form action={deleteSnippetAction}>
+            <Button variant={"destructive"} type="submit">
+              Delete
+            </Button>
+          </form>
         </div>
       </div>
       <pre className="p-3 bg-gray-200 rounded border-gray-200">
